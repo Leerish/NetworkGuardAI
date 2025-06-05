a4267c32ecfc4e08b5703b00d2c9a1906c946473
Leerish Arvind <arvindleerish@gmail.com>
2025-06-05 20:20:52 +0530

    Model Training Completed and initiated MLflow for tracking

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -14,7 +14,8 @@ FILE_NAME: str = "data.csv"
 TRAIN_FILE_NAME:str ="train.csv"
 TEST_FILE_NAME:str = "test.csv"
 SCHEMA_FILE_PATH = os.path.join("data_schema" , "schema.yaml")
-
+MODEL_FILE_NAME = "model.pkl"
+SAVED_MODEL_DIR =os.path.join("saved_models")
 """
 Data Ingestion related content  start with DATA_INGESTION VAR NAME 
 """
@@ -49,3 +50,16 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     "n_neighbors" : 3,
     "weights" : "uniform",
 }
+
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
+DATA_TRANSFORMATION_TEST_FILE_PATH:str = "test.npy"
+
+"""
+Model trainer related constant start with MODEL TRAINER VAR NAME
+"""
+
+MODEL_TRAINER_DIR_NAME:str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR:str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME:str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE:float =  0.6
+MODEL_TRAINER_OVER_FITTING_UNDERFITTING_THRESHOLD:float = 0.05
